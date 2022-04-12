3cfa285a73b213e6999399f17aee41f14423b5d7
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    sm8150-common: Pick up USB configurations from QCOM
     * If we want to override anything in particular, e.g.
       PID/VIDs we will create a seperate script named
       'init.xiaomi.usb.rc'.
    
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- msmnile.mk
+++ msmnile.mk
@@ -370,6 +370,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
