62ca75b56bd1a932904d385d1bf75123bc34c996
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    sm8150-common: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,6 +93,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
 TARGET_KERNEL_CONFIG := \
     vendor/sm8150-perf_defconfig \
     vendor/xiaomi/sm8150-common.config
+TARGET_KERNEL_NO_GCC := true
 
 # Media
 TARGET_USES_ION := true
