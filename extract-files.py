64c30cb702dd8c1f7d4193cd2432bc4c0c9c186a
Suren Baghdasaryan <surenb@google.com>
2025-06-30 12:08:56 +0530

    sm8150-common: Replace writepid with task_profiles command
    writepid command usage to join a cgroup has been deprecated in favor
    of a more flexible approach using task_profiles. This way cgroup path
    is not hardcoded and cgroup changes can be easily made. Replace
    writepid with task_profiles command to migrate between cgroups.
    
    Bug: 191283136
    Test: build and boot
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ibb56d2379c42e5ed4b2f15c1b04d5fb163b48678

--- extract-files.py
+++ extract-files.py
@@ -25,8 +25,14 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
+    'vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc': blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles ProcessCapacityHigh HighPerformance'),
+    'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc': blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles NNApiHALPerformance'),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
+    'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc': blob_fixup()
+        .regex_replace(r'writepid.*', 'task_profiles ProcessCapacityHigh HighPerformance'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
     'vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
